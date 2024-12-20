1d41bc471133f163061d782626e7dce35cccbe79
yarenunal <yareunal16@gmail.com>
2024-12-20 20:11:29 +0300

    20aralık

---
+++ WeatherApp/Preview Content/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  WeatherApp
+//
+//  Created by Yaren on 20.12.2024.
+//
+
+import Foundation
---
+++ WeatherApp/Views/WeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherView.swift
+//  WeatherApp
+//
+//  Created by Yaren on 20.12.2024.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
